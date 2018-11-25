4fc024c698a8748862fd81c0481e285d73f99af3
Ervin <ervin@github.com>
2018-11-25 21:38:20 +0100

    First

--- test.py
+++ test.py
@@ -6,8 +6,11 @@ import csv
 parser = argparse.ArgumentParser()
 parser.add_argument("-l", help="Affiche le contenu de la liste", action="store_true")
 parser.add_argument("-a", type=int, help="Ajoute les elements dans liste.csv", nargs='+')
-parser.add_argument("-c", help="Supprime tous les elements de la liste", action="store_true")
-parser.add_argument("-s", "--max", help="Affiche la valeur maximum de la liste", action="store_true")
+parser.add_argument("-c", help="Supprime tous les elements dans liste.csv", action="store_true")
+parser.add_argument("-s", "--option", help="--max / --min / --moy", action="store_true")
+parser.add_argument("--max", help="Affiche la valeur maximale dans liste.csv", action="store_true")
+parser.add_argument("--min", help="Affiche la valeur minimale dans liste.csv", action="store_true")
+parser.add_argument("--moy", help="Affiche la moyenne des valeurs de liste.csv", action="store_true")
 
 args = parser.parse_args()
 
@@ -30,21 +33,63 @@ elif args.c:
     truncate_list = open("liste.csv", "w+")
     truncate_list.close()
 
-elif args.max:
+elif args.option:
 
-    with open('liste.csv', 'r') as liste:
-        reader = csv.reader(liste, dialect='excel')
+    if args.max:
 
-        tab = []
+        with open('liste.csv', 'r') as liste:
+            
+            reader = csv.reader(liste)
 
-        for row in reader:
-            for x in row:
-                x = int(x)
-                tab.append(x)
+            liste = []
 
-        yo = max(tab)
-        print(yo)
+            for row in reader:
+                
+                for x in row:
+                
+                    x = int(x)
+                    liste.append(x)
 
+            max_value = max(liste)
+            print('Valeur maximale : ', max_value)
+
+    elif args.min:
+
+        with open('liste.csv', 'r') as liste:
+            
+            reader = csv.reader(liste)
+
+            liste = []
+
+            for row in reader:
+                
+                for x in row:
+                
+                    x = int(x)
+                    liste.append(x)
+
+            min_value = min(liste)
+            print('Valeur minimale : ', min_value)
+        
+    elif args.moy:
+
+        with open('liste.csv', 'r') as liste:
+            
+            reader = csv.reader(liste)
+
+            liste = []
+
+            for row in reader:
+                
+                for x in row:
+                
+                    x = int(x)
+                    liste.append(x)
+
+            avg = sum(liste)/len(liste)
+            print('Moyenne : ', avg)
+            print(sum(liste))
+            print(len(liste))
 
 
         
