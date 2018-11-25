37c7718e4ddec349fa742851bd263588e16fef01
Ervin <ervin@github.com>
2018-11-25 22:26:16 +0100

    First

--- SALIF_Ervin_kholle_1.py
+++ SALIF_Ervin_kholle_1.py
@@ -3,6 +3,8 @@
 import argparse
 import csv
 
+# Parsing des options de commandes CLI
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-l", help="Affiche le contenu de la liste", action="store_true")
 parser.add_argument("-a", type=int, help="Ajoute les elements dans liste.csv", nargs='+')
@@ -12,12 +14,14 @@ parser.add_argument("--max", help="Affiche la valeur maximale dans liste.csv", a
 parser.add_argument("--min", help="Affiche la valeur minimale dans liste.csv", action="store_true")
 parser.add_argument("--moy", help="Affiche la moyenne des valeurs de liste.csv", action="store_true")
 parser.add_argument("--sum", help="Affiche la somme des valeurs de liste.csv", action="store_true")
-parser.add_argument("-t", help="Trie liste.csv dans l'ordre croissant", action="store_true")
-parser.add_argument("--desc", help="Trie liste.csv dans l'ordre décroissant", action="store_true")
+parser.add_argument("-t", help="Trie liste.csv dans l'ordre croissant / --desc", action="store_true")
+parser.add_argument("--desc", help="Trie liste.csv dans l'ordre decroissant", action="store_true")
 
 
 args = parser.parse_args()
 
+# Si <nom_du_script> -l / Affiche tous les elements contenus dans liste.csv 
+ 
 if args.l:
     
     with open('liste.csv', 'r') as csvfile:
@@ -26,11 +30,15 @@ if args.l:
         for row in reader:
             print(' '.join(row))
 
+# Si <nom_du_script> -a / Ajoute les arguments de type int dans liste.csv 
+
 elif args.a:
 
     with open('liste.csv', 'a') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerows([[a] for a in args.a])
+
+# Si <nom_du_script> -c / Supprime tous les elements contenus dans liste.csv 
 
 elif args.c:
 
@@ -38,6 +46,8 @@ elif args.c:
     truncate_list.close()
 
 elif args.s:
+
+    # Si <nom_du_script> -s --max / Affiche la valeur maximale contenu dans liste.csv
 
     if args.max:
 
@@ -56,6 +66,8 @@ elif args.s:
 
             max_value = max(liste)
             print('Valeur maximale : ', max_value)
+    
+    # Si <nom_du_script> -s --min / Affiche la valeur minimale contenu dans liste.csv
 
     elif args.min:
 
@@ -74,7 +86,9 @@ elif args.s:
 
             min_value = min(liste)
             print('Valeur minimale : ', min_value)
-        
+
+    # Si <nom_du_script> -s --moy / Affiche la moyenne de tous les elements contenus dans liste.csv
+    
     elif args.moy:
 
         with open('liste.csv', 'r') as csvfile:
@@ -94,6 +108,8 @@ elif args.s:
             
             print('Moyenne : ', avg)
     
+    # Si <nom_du_script> -s --sum / Affiche la somme de tous les elements contenus dans liste.csv
+
     elif args.sum:
 
         with open('liste.csv', 'r') as csvfile:
@@ -113,7 +129,11 @@ elif args.s:
             
             print('Somme : ', summary)
             
+# Si <nom_du_script> -t / Affiche tous les elements contenus dans liste.csv dans l'ordre croissant
+
 elif args.t:
+
+        # Si <nom_du_script> -t --desc / Affiche tous les elements contenus dans liste.csv dans l'ordre décroissant
 
         if args.desc:
 
